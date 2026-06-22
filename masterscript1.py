5a12a13c7ff4561e825a49a746fc5040cab8fd86
Kailey McGady <kmcgads01@gmail.com>
2026-06-22 14:27:16 -0500

    experiment one masterscript

---
+++ masterscript1.py
@@ -0,0 +1,7 @@
+"""This is where I will put all of the prior coding together to program in a color mixing experiment"""
+#step 1, load all three stash colors and hold them in place (20x20) at start spots of 4,4;4,56;4,104
+
+#step 2, have opt choose a random color and random VOLUME to take of each of the colors
+
+#step 3, split off random volumes from each color
+
